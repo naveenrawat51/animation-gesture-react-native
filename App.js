4141a68aba9b1db4725e914043ef00f62c91992e
Naveen Rawat <naveen.rawat@sprint.com>
2020-12-26 19:43:46 +0530

    current swippepd card replaced by new card logic added

--- App.js
+++ App.js
@@ -1,93 +1,98 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Animated, View } from "react-native";
-import Ball from "./components/Ball";
-import Deck from "./components/Deck";
-import { Card, Text, Button, Icon } from "react-native-elements";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Animated, View } from 'react-native';
+import Ball from './components/Ball';
+import Deck from './components/Deck';
+import { Card, Text, Button, Icon } from 'react-native-elements';
 
 const DATA = [
-  {
-    id: 1,
-    text: "Card #1",
-    uri: "https://randomuser.me/api/portraits/men/71.jpg",
-  },
-  {
-    id: 2,
-    text: "Card #2",
-    uri: "https://randomuser.me/api/portraits/men/72.jpg",
-  },
-  {
-    id: 3,
-    text: "Card #3",
-    uri: "https://randomuser.me/api/portraits/men/73.jpg",
-  },
-  {
-    id: 4,
-    text: "Card #4",
-    uri: "https://randomuser.me/api/portraits/men/74.jpg",
-  },
-  {
-    id: 5,
-    text: "Card #5",
-    uri: "https://randomuser.me/api/portraits/men/75.jpg",
-  },
-  {
-    id: 6,
-    text: "Card #6",
-    uri: "https://randomuser.me/api/portraits/men/76.jpg",
-  },
-  {
-    id: 7,
-    text: "Card #7",
-    uri: "https://randomuser.me/api/portraits/men/77.jpg",
-  },
-  {
-    id: 8,
-    text: "Card #8",
-    uri: "https://randomuser.me/api/portraits/men/78.jpg",
-  },
+    {
+        id: 1,
+        text: 'Card #1',
+        uri: 'https://randomuser.me/api/portraits/men/71.jpg',
+    },
+    {
+        id: 2,
+        text: 'Card #2',
+        uri: 'https://randomuser.me/api/portraits/men/72.jpg',
+    },
+    {
+        id: 3,
+        text: 'Card #3',
+        uri: 'https://randomuser.me/api/portraits/men/73.jpg',
+    },
+    {
+        id: 4,
+        text: 'Card #4',
+        uri: 'https://randomuser.me/api/portraits/men/74.jpg',
+    },
+    {
+        id: 5,
+        text: 'Card #5',
+        uri: 'https://randomuser.me/api/portraits/men/75.jpg',
+    },
+    {
+        id: 6,
+        text: 'Card #6',
+        uri: 'https://randomuser.me/api/portraits/men/76.jpg',
+    },
+    {
+        id: 7,
+        text: 'Card #7',
+        uri: 'https://randomuser.me/api/portraits/men/77.jpg',
+    },
+    {
+        id: 8,
+        text: 'Card #8',
+        uri: 'https://randomuser.me/api/portraits/men/78.jpg',
+    },
 ];
 
 export default function App() {
-  let [position, setPosition] = useState(new Animated.ValueXY(0, 0));
+    let [position, setPosition] = useState(new Animated.ValueXY(0, 0));
 
-  const renderCard = (item) => (
-    <Card key={item.id}>
-      <Card.Image source={{ uri: item.uri }} />
-      <Text style={{ marginVertical: 10 }}>{item.text}</Text>
-      <Button
-        icon={<Icon name="code" color="#ffffff" />}
-        buttonStyle={{
-          borderRadius: 0,
-          marginLeft: 0,
-          marginRight: 0,
-          marginBottom: 0,
-        }}
-        title="VIEW NOW"
-      />
-    </Card>
-  );
+    const renderCard = (item) => (
+        <Card key={item.id}>
+            <Card.Image source={{ uri: item.uri }} />
+            <Text style={{ marginVertical: 10 }}>{item.text}</Text>
+            <Button
+                icon={<Icon name="code" color="#ffffff" />}
+                buttonStyle={{
+                    borderRadius: 0,
+                    marginLeft: 0,
+                    marginRight: 0,
+                    marginBottom: 0,
+                }}
+                title="VIEW NOW"
+            />
+        </Card>
+    );
 
-  useEffect(() => {
-    Animated.spring(position, {
-      toValue: { x: 200, y: 500 },
-      useNativeDriver: false,
-    }).start();
-  }, []);
+    useEffect(() => {
+        Animated.spring(position, {
+            toValue: { x: 200, y: 500 },
+            useNativeDriver: false,
+        }).start();
+    }, []);
 
-  return (
-    // <Animated.View style={position.getLayout()}>  enable ball component specific code if you wish to check ball animation
-    //   <Ball />
-    // </Animated.View>
-    <View style={styles.container}>
-      <Deck data={DATA} renderCard={renderCard} />
-    </View>
-  );
+    return (
+        // <Animated.View style={position.getLayout()}>  enable ball component specific code if you wish to check ball animation
+        //   <Ball />
+        // </Animated.View>
+        <View style={styles.container}>
+            <Deck
+                data={DATA}
+                renderCard={renderCard}
+                onSwipeLeft={() => {}}
+                onSwipeRight={() => {}}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    marginVertical: 50,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginVertical: 50,
+    },
 });
--- components/Deck.js
+++ components/Deck.js
@@ -1,29 +1,32 @@
-import React, {useState} from 'react';
-import {View, Animated, PanResponder, Dimensions} from 'react-native';
+import React, { useState } from 'react';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
-export default function Deck({data, renderCard, onSwipeLeft, onSwipeRight}) {
-    const [useState, setIndex] = useState();
+export default function Deck({ data, renderCard, onSwipeLeft, onSwipeRight }) {
+    const [index, setIndex] = useState(0);
     const position = new Animated.ValueXY();
 
     const resetPosition = () => {
         Animated.spring(position, {
-            toValue: {x: 0, y: 0},
+            toValue: { x: 0, y: 0 },
             useNativeDriver: false,
         }).start();
     };
 
     const onSwipeComplete = (direction) => {
-        direction === right ? onSwipeRight() : onSwipeLeft();
+        const item = data[index];
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        position.setValue({ x: 0, y: 0 });
+        setIndex(index + 1);
     };
 
     const forceSwipe = (direction) => {
         const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
         Animated.timing(position, {
-            toValue: {x, y: 0},
+            toValue: { x, y: 0 },
             duration: SWIPE_OUT_DURATION,
             useNativeDriver: false,
         }).start(() => onSwipeComplete(direction));
@@ -32,7 +35,7 @@ export default function Deck({data, renderCard, onSwipeLeft, onSwipeRight}) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderMove: (event, gesture) => {
-            position.setValue({x: gesture.dx, y: gesture.dy});
+            position.setValue({ x: gesture.dx, y: gesture.dy });
         },
         onPanResponderRelease: (event, gesture) => {
             if (gesture.dx > SWIPE_THRESHOLD) {
@@ -55,13 +58,16 @@ export default function Deck({data, renderCard, onSwipeLeft, onSwipeRight}) {
 
         return {
             ...position.getLayout(),
-            transform: [{rotate: rotate}],
+            transform: [{ rotate: rotate }],
         };
     };
 
     const renderCards = () =>
-        data.map((item, index) => {
-            if (index === 0) {
+        data.map((item, ind) => {
+            if (ind < index) {
+                return null;
+            }
+            if (ind === index) {
                 return (
                     <Animated.View
                         key={item.id}
