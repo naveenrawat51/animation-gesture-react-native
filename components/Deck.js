1c727f3754eed943013a85ce57e7475ca259d60c
Naveen Rawat <naveen.rawat@sprint.com>
2020-12-26 14:40:34 +0530

    user card left and right swipe added

--- components/Deck.js
+++ components/Deck.js
@@ -1,8 +1,74 @@
 import React from "react";
-import { View, Animated } from "react-native";
+import { View, Animated, PanResponder, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 export default function Deck({ data, renderCard }) {
-  const renderCards = () => data.map((item) => renderCard(item));
+  const position = new Animated.ValueXY();
+
+  const resetPosition = () => {
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const forceSwipe = (direction) => {
+    const x = direction === "right" ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gesture) => {
+      position.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+    onPanResponderRelease: (event, gesture) => {
+      if (gesture.dx > SWIPE_THRESHOLD) {
+        console.log("swipe right");
+        forceSwipe("right");
+      } else if (gesture.dx < -SWIPE_THRESHOLD) {
+        console.log("swipe left");
+        forceSwipe("left");
+      } else {
+        resetPosition();
+      }
+    },
+  });
+
+  const getCardStyle = () => {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ["-120deg", "0deg", "120deg"],
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate: rotate }],
+    };
+  };
+
+  const renderCards = () =>
+    data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={getCardStyle()}
+            {...panResponder.panHandlers}
+          >
+            {renderCard(item)}
+          </Animated.View>
+        );
+      }
+      return renderCard(item);
+    });
 
   return <View>{renderCards()}</View>;
 }
